11f3eaf15defc418c97801c00bc17c2e38025a99
Augustas Pocius <nesuspameris@gmail.com>
2017-11-20 17:08:29 +0200

    Added main files

---
+++ Assets/RotationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class RotationScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+            
+	}
+
+    public void Rotate(float value)
+    {
+       
+    }
+
+    bool CheckIfNotEmpty(GameObject gameobj)
+    {
+        if(gameobj)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+}
